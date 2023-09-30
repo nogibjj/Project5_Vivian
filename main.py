3b6cf25593b986a842eba407d39c60d812381a7d
vivianzzzzz <143654445+vivianzzzzz@users.noreply.github.com>
2023-09-30 11:19:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 from lib.CRUD import (
-close_connection,create_house,update_house,delete_house,read_all_houses)
+    close_connection,
+    create_house,
+    update_house,
+    delete_house,
+    read_all_houses,
+    read_house_by_id
+)
 from lib.load_data import load
 
-def main():
-    # Example Usage
-    load('california_housing_train.csv')  # Import data from CSV
-    create_house(-122.5, 37.7, 40, 2000, 350, 1500, 350, 8.5, 500000)  # Create
-    houses = read_all_houses()  # Read
-    print("All Houses:", houses)
 
-    # Assuming the ID of the house to update is 1
+def main():
+    load("california_housing_train.csv")  # Import data from CSV
+
+    #create_house
+    create_house(-122.5, 37.7, 40, 2000, 350, 1500, 350, 8.5, 500000)  # Create
+
+    # update house with id=1
     update_house(1, -122.5, 37.7, 45, 2500, 400, 1600, 400, 9.0, 550000)  # Update
 
-    # Get and print all houses after update
-    updated_houses = read_all_houses()  # Read after update
-    print("Updated Houses:", updated_houses)
+    # print house with id=10
+    read_house_by_id(10)  # Read after update
 
-    # Assuming the ID of the house to delete is 1
+    # delete house with id=5
     delete_house(1)  # Delete
-
-    # Get and print all houses after deletion
-    houses_after_deletion = read_all_houses()  # Read after deletion
-    print("Houses after Deletion:", houses_after_deletion)
 
     # Close the connection
     close_connection()
-    return 1 
-    
+    return 1
+
+
 if __name__ == "__main__":
     main()
