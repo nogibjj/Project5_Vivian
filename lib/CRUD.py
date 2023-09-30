e19c32e5f3c47ab7454a4cccec63de4e800fcb7e
vivianzzzzz <143654445+vivianzzzzz@users.noreply.github.com>
2023-09-30 11:20:25 -0400

    Update CRUD.py

--- lib/CRUD.py
+++ lib/CRUD.py
@@ -9,22 +9,34 @@ def create_house(longitude, latitude, housing_median_age, total_rooms, total_bed
     cursor.execute("INSERT INTO houses (longitude, latitude, housing_median_age, total_rooms, total_bedrooms, population, households, median_income, median_house_value) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", 
                    (longitude, latitude, housing_median_age, total_rooms, total_bedrooms, population, households, median_income, median_house_value))
     conn.commit()
+    numbers=[longitude, latitude, housing_median_age, total_rooms, total_bedrooms, population, households, median_income, median_house_value]
+    print("House successfully create: "+','.join(map(str, numbers)))
 
 # READ: Get all houses
 def read_all_houses():
     cursor.execute("SELECT * FROM houses")
     return cursor.fetchall()
 
-# UPDATE: Update a house's data based on its ID (assuming you've an ID or primary key column)
+def read_house_by_id(house_id):
+    cursor.execute("SELECT * FROM houses WHERE id=?", (house_id,))
+    # Fetch the result
+    house = cursor.fetchone()
+    print("house with id="+str(house_id)+" has these info: "+house)
+
+
+# UPDATE: Update a house's data based on its ID 
 def update_house(id, longitude, latitude, housing_median_age, total_rooms, total_bedrooms, population, households, median_income, median_house_value):
     cursor.execute("UPDATE houses SET longitude=?, latitude=?, housing_median_age=?, total_rooms=?, total_bedrooms=?, population=?, households=?, median_income=?, median_house_value=? WHERE id=?", 
                    (longitude, latitude, housing_median_age, total_rooms, total_bedrooms, population, households, median_income, median_house_value, id))
     conn.commit()
+    numbers=[longitude, latitude, housing_median_age, total_rooms, total_bedrooms, population, households, median_income, median_house_value]
+    print("House with id "+str(id)+" successfullu updated to:"+','.join(map(str, numbers)))
 
 # DELETE: Delete a house by its ID
 def delete_house(id):
     cursor.execute("DELETE FROM houses WHERE id=?", (id,))
     conn.commit()
+    print("House with id "+str(id)+" deleted!")
 
 # Close the connection (Make sure to call this when done with database operations)
 def close_connection():
